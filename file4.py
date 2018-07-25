38bd7efd2583d1dbaec4f0c8398a79960be28ab3
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-25 03:32:45 -0400

    added file4

---
+++ file4.py
@@ -0,0 +1,7 @@
+rate=float(input("enter the dollar value:"))
+dollar=int(input("enter the dollar number:"))
+inr=dollar*rate
+#amt=float(user)
+#convert=amt*68.80
+print("the inr",dollar)
+print("the rate",inr)
